4b4596dd12892db634403ebb6ef38c3b35783ffa
Chanry <chanry@SHUAIdeiMac.fios-router.home>
2020-09-11 17:22:18 -0400

    added a few branches

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './components/login/login.component';
+import { SuperheroComponent } from './components/superhero/superhero.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NavbarComponent,
+    SuperheroComponent
   ],
   imports: [
     BrowserModule,
